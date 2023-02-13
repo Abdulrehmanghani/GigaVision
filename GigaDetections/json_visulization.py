738cc76329774f5694665dff3502dd418c13a8d9
Abdulrehmanghani <amaizingboy197@gmail.com>
2023-02-13 20:07:52 +0500

    code modeified

--- GigaDetections/json_visulization.py
+++ GigaDetections/json_visulization.py
@@ -7,19 +7,23 @@ import os
 import json
   
 # Opening JSON file
-f = open('instances_train2017.json')
-
+f = open('instances_train2017_split.json')
+classes = ['none','person','vehicle']
 data = json.load(f)
 for i in range(len(data['images'])):
     imge = data["images"][i]['file_name']
-    
-    img = cv2.imread('train2017/'+imge)
+    try:
+        img = cv2.imread('../val/'+imge)
+    except:
+        continue
     ids = data["images"][i]['id']
-
-    for j in range(len(data["annotations"])):
-        if ids == data["annotations"][j]['image_id']:
-           cat_id=data["annotations"][j]['category_id'] 
     
+    for j in range(len(data["annotations"])):
+
+        if ids == data["annotations"][j]['image_id']:
+
+            cat_id=data["annotations"][j]['category_id'] 
+
             try:
                 xmin=int(data["annotations"][j]['bbox'][0])
                 ymin=int(data["annotations"][j]['bbox'][1])
@@ -28,8 +32,8 @@ for i in range(len(data['images'])):
             except:
                 continue
             cv2.rectangle(img,(xmin,ymin),
-                                  (xmax,ymax),(0,0,255),10)
-            cv2.putText(img, str(cat_id), (int(xmin)-5, int(ymin)-15), cv2.FONT_HERSHEY_SIMPLEX,  
-                           10, (255,0,0), 10, cv2.LINE_AA)
+                                    (xmax,ymax),(0,0,255),5)
+            cv2.putText(img, str(classes[cat_id]), (int(xmin)-5, int(ymin)-15), cv2.FONT_HERSHEY_SIMPLEX,  
+                           2, (255,0,0), 2, cv2.LINE_AA)
       
     cv2.imwrite("testimg/"+imge,img)
